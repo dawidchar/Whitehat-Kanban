126a12c32127e75bfd9acb35560e35c38a485c00
dawidchar <dawidchar1@gmail.com>
2020-10-28 15:51:14 +0000

    Server REST and Handlebars Template 🚲💻

--- index.js
+++ index.js
@@ -12,8 +12,93 @@ app.use(express.static('public'))
 app.engine('handlebars', handlebars)
 app.set('view engine', 'handlebars')
 
-app.get('/', (request, response) => {
-    response.render('restaurants', {date: new Date()})
+app.get('/', (request, response) => { //Login Page
+    response.render('login', {date: new Date()})
 })
+
+app.get('/boards', (request, response) => { //All Boards Page
+    
+})
+
+app.get('/myboards', (request, response) => { //Boards You are part of Page
+    
+})
+
+app.get('/board/:id', (request, response) => { // Specific Board Page
+    
+})
+
+
+
+///////////// REST /////////////
+
+////Users
+
+app.get('/api/users', (request, response) => { //Get All Users
+
+})
+
+app.post('/api/users', (request, response) => { // Create New User
+
+})
+
+app.get('/api/users/:userid', (request, response) => { //Get User with ID
+
+})
+
+app.post('/api/users/:userid', (request, response) => { // Update User with that ID
+
+})
+
+app.post('/api/users/:userid/delete', (request, response) => { // Delete User With That ID
+
+})
+
+////Boards
+
+app.get('/api/boards', (request, response) => { //Get All Boards
+
+})
+
+app.post('/api/boards', (request, response) => { //Create New Board
+
+})
+
+app.get('/api/board/:id', (request, response) => { //Get Board With ID
+
+})
+
+app.post('/api/board/:id', (request, response) => { //Update Board with that ID
+
+})
+
+app.post('/api/board/:id/delete', (request, response) => { //Delete Board With that ID
+
+})
+
+//// Tasks
+
+app.get('/api/board/:id/tasks', (request, response) => { // Get Tasks From the Board With that Board ID
+
+})
+
+app.post('/api/board/:id/tasks', (request, response) => {// Create a New Task For the Board with that Board ID
+
+})
+
+//(Each task has a unique ID regardless of their board so we can just refrence it)
+
+app.get('/api/task/:taksid', (request, response) => { // Get A Single Task 
+
+})
+
+app.post('/api/task/:taksid', (request, response) => {// Update a Specific Task with that Task ID
+
+})
+
+app.post('/api/task/:taksid/delete', (request, response) => { // Delete Task With that Task ID
+
+})
+
 
 app.listen(3000, () => console.log('web server running on port 3000'))
