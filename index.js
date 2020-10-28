2047ec5be6783c70a90924d5731402f1a728de53
dawidchar <dawidchar1@gmail.com>
2020-10-28 15:23:07 +0000

    Inital Server Setup 🚀

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const Handlebars = require('handlebars')
+const expressHandlebars = require('express-handlebars')
+const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access')
+const app = express()
+
+const handlebars = expressHandlebars({
+    handlebars: allowInsecurePrototypeAccess(Handlebars)
+})
+
+app.use(express.static('public'))
+app.engine('handlebars', handlebars)
+app.set('view engine', 'handlebars')
+
+app.get('/', (request, response) => {
+    response.render('restaurants', {date: new Date()})
+})
+
+app.listen(3000, () => console.log('web server running on port 3000'))
