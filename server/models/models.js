359f04a29598519f5a019b2715fd4bcc7ddf4c53
dawidchar <dawidchar1@gmail.com>
2020-10-29 21:20:00 +0000

    Format and Add Alias to models 🗽

--- server/models/models.js
+++ server/models/models.js
@@ -1,32 +1,32 @@
 const { Sequelize, Model, DataTypes } = require('sequelize');
 const path = require('path')
 const sequelize = process.env.NODE_ENV === 'test'
-    ? new Sequelize('sqlite::memory:', null, null, {dialect: 'sqlite', logging: false})
-    : new Sequelize({dialect: 'sqlite', storage: path.join(__dirname, '../database.sqlite'), logging: false})
+    ? new Sequelize('sqlite::memory:', null, null, { dialect: 'sqlite', logging: false })
+    : new Sequelize({ dialect: 'sqlite', storage: path.join(__dirname, '../database.sqlite'), logging: false })
 
-class Board extends Model {}
+class Board extends Model { }
 Board.init({
     title: DataTypes.STRING
-}, {sequelize})
+}, { sequelize })
 
-class User extends Model {}
+class User extends Model { }
 User.init({
     name: DataTypes.STRING,
     avatar: DataTypes.STRING
-}, {sequelize})
+}, { sequelize })
 
-class Task extends Model {}
+class Task extends Model { }
 Task.init({
     name: DataTypes.STRING
-}, {sequelize})
+}, { sequelize })
 
-User.belongsToMany(Board, {through: 'User_Board'})
-Board.belongsToMany(User, {through: 'User_Board'})
+User.belongsToMany(Board, { as: 'boards', through: 'User_Board' })
+Board.belongsToMany(User, { as: 'users', through: 'User_Board' })
 
-Board.hasMany(Task, {as: 'tasks'})
+Board.hasMany(Task, { as: 'tasks' })
 Task.belongsTo(Board)
 
-User.hasMany(Task, {as: 'tasks'})
+User.hasMany(Task, { as: 'tasks' })
 Task.belongsTo(User)
 
 module.exports = { Board, User, Task, sequelize }
