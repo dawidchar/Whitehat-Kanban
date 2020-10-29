db21bb890480edc2557dc8ac917367a849f5d2e1
Taffanie Ben <46627573+taffanie@users.noreply.github.com>
2020-10-29 10:27:22 +0000

    sequelize models added (#30)
    * models added
    
    * Update models.js
    
    Co-authored-by: github username <github email address>

---
+++ models/models.js
@@ -0,0 +1,32 @@
+const { Sequelize, Model, DataTypes } = require('sequelize');
+const path = require('path')
+const sequelize = process.env.NODE_ENV === 'test'
+    ? new Sequelize('sqlite::memory:', null, null, {dialect: 'sqlite', logging: false})
+    : new Sequelize({dialect: 'sqlite', storage: path.join(__dirname, '../server/database.sqlite'), logging: false})
+
+class Board extends Model {}
+Board.init({
+    title: DataTypes.STRING
+}, {sequelize})
+
+class User extends Model {}
+User.init({
+    name: DataTypes.STRING,
+    avatar: DataTypes.STRING
+}, {sequelize})
+
+class Task extends Model {}
+Task.init({
+    name: DataTypes.STRING
+}, {sequelize})
+
+User.belongsToMany(Board, {through: 'User_Board'})
+Board.belongsToMany(User, {through: 'User_Board'})
+
+Board.hasMany(Task, {as: 'tasks'})
+Task.belongsTo(Board)
+
+User.hasMany(Task, {as: 'tasks'})
+Task.belongsTo(User)
+
+module.exports = { Board, User, Task, sequelize }
