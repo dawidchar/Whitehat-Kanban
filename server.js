5b0e474dd0fb714524fba967a99710c64606eff2
dawidchar <dawidchar1@gmail.com>
2020-10-30 00:59:06 +0000

    Fully Functional REST Endpoints 🚀🤘😁

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const Handlebars = require('handlebars')
 const expressHandlebars = require('express-handlebars')
 const bodyParser = require('body-parser')
 const { Board, User, Task, sequelize } = require('./server/models/models.js');
-const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access')
+const { allowInsecurePrototypeAccess } = require('@handlebars/allow-prototype-access')
 
 const app = express()
 
@@ -14,38 +14,29 @@ const handlebars = expressHandlebars({
 app.use(express.static('public'))
 app.engine('handlebars', handlebars)
 app.set('view engine', 'handlebars')
-app.use(express.urlencoded({ extended: true}))
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
-app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.json());       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-  extended: true
-})); 
+    extended: true
+}));
 
-app.get('/', (request, response) => { //Login Page
-    response.render('login', {date: new Date()})
+app.get('/', (req, res) => { //Login Page
+    res.render('login')
 })
 
-app.get('/boards', async (request, response) => { //All Boards Page
-    const boards = await Board.findAll({
-        include: 'users',
-        nest: true 
-    })
-    response.render('boards', {boards})
+app.get('/boards', (req, res) => { //All Boards Page
+    res.render('boards')
 })
 
-app.get('/:userid/myboards', async (request, response) => { //Boards You are part of Page
-    const user = await User.findByPk(request.params.userid)
-    const boards = await user.getBoards()
-    response.render('myboards', {user, boards})
+app.get('/:userid/myboards', (req, res) => { //Boards You are part of Page
+    res.render('myboards')
 })
 
 
-app.get('/board/:id', async (request, response) => { // Specific Board Page
-    const board = await Board.findByPk(request.params.id)
-    const tasks = await board.getTasks()
-    const users = await board.getUsers()
-    response.render('board', {board, tasks, users})
+app.get('/board/:id', (req, res) => { // Specific Board Page
+    res.render('board')
 })
 
 
@@ -53,179 +44,190 @@ app.get('/board/:id', async (request, response) => { // Specific Board Page
 
 ////Users
 
-app.get('/api/users', (request, response) => { //Get All Users
-    const users = User.all
-    response.send(users)
+app.get('/api/users', async (req, res) => { //Get All Users
+    const users = await User.findAll()
+    res.send(users)
 })
 
-app.post('/api/users', (request, response) => { // Create New User
-    User.all.push(request.body)
-    response.send()
+app.post('/api/users', async (req, res) => { // Create New User
+    await User.create(req.body)
+    res.send(true)
 })
 
-app.get('/api/users/:userid', async (request, response) => { //Get User with ID
-    const user = await User.findByPk(request.params.userid)
-    response.send(user)
+app.get('/api/users/:userid', async (req, res) => { //Get User with ID
+    const user = await User.findByPk(req.params.userid)
+    res.send(user)
 })
 
-app.get('/api/users/:userid/boards', async (request, response) => { //Get the Boards of the User with ID
-    const user = await User.findByPk(request.params.userid)
+app.get('/api/users/:userid/boards', async (req, res) => { //Get the Boards of the User with ID
+    const user = await User.findByPk(req.params.userid)
     const boards = await user.getBoards()
-    response.send(boards)
+    res.send(boards)
 })
 
-app.post('/api/users/:userid', async (request, response) => { // Update User with that ID
-    const user = await User.findByPk(request.params.userid)
-    await user.update(request.body)
-    response.send(user)
+app.post('/api/users/:userid', async (req, res) => { // Update User with that ID
+    await User.update(req.body, {
+        where: { id: req.params.userid }
+    })
+    res.send(true)
 })
 
-app.post('/api/users/:userid/delete', async (request, response) => { // Delete User With That ID
-    const user = await User.findByPk(request.params.userid)
-    await user.destroy()
-    response.send()
+app.post('/api/users/:userid/delete', async (req, res) => { // Delete User With That ID
+    await User.destroy({
+        where: { id: req.params.userid }
+    })
+    res.send(true)
 })
 
 ////Boards
 
-app.get('/api/boards', async (request, response) => { //Get All Boards
-    let boards = await Board.findAll();
-    response.send(JSON.stringify(boards, null, 2))
+app.get('/api/boards', async (req, res) => { //Get All Boards
+    let boards = await Board.findAll({
+        include: { model: User, as: "users" }
+    });
+    res.send(boards)
 })
 
-app.post('/api/boards', async (request, response) => { //Create New Board
-    if (request.body.bName) {
-        let bName = request.body.bName;
-        let userId = request.body.userid;
+app.post('/api/boards', async (req, res) => { //Create New Board
+    if (req.body.title && req.body.userid) {
         const user = await User.findOne({
-            where: { id: userId}
+            where: { id: req.body.userid }
         });
-        const board = await Board.create({title: bName})
-        let handler = await user.addBoard(board);
-        response.send(true);
+        const board = await Board.create({ title: req.body.title })
+        await user.addBoard(board);
+        res.send(true);
     } else {
-        response.send(false);
-    }
-    
-})
-
-
-app.get('/api/board/:id', async (request, response) => { //Get Board With ID
-    if (request.params.id) {
-        let id = request.params.id;
-        let board = await Board.findOne({
-            where: {id: id}
-        });
-        response.send(board)
-    } else {
-        response.send({});
+        res.send(false);
     }
 })
 
-app.post('/api/board/:id', async (request, response) => { //Update Board with that ID
-    if (request.params.id) {
-        let id = request.params.id;
-        let operation = request.body.operation;
-        let userid = request.body.userid;
-        let board = await Board.findOne({
-            where: {id: id}
-        });
-        let user = await User.findOne({
-            where: {id: userid}
-        });
-        switch (operation) {
-            case 'adduser':
-                board.addUser(user);
-                response.send(true);
-                break;
-            case 'removeuser':
-                board.removeUser(user);
-                response.send(true)
-                break;  
-            default:
-                response.send(false)
-                break;
-        }
+
+app.get('/api/board/:id', async (req, res) => { //Get Board With ID
+    let board = await Board.findOne({
+        where: { id: req.params.id },
+        include: { model: User, as: "users" }
+    });
+    res.send(board)
+})
+
+app.post('/api/board/:id', async (req, res) => { //Update Board with that ID
+    res.send(false)
+})
+
+app.post('/api/board/:id/adduser/:userid', async (req, res) => { //Update Board -- Add User
+    let board = await Board.findOne({
+        where: { id: req.params.id }
+    });
+    let user = await User.findOne({
+        where: { id: req.params.userid }
+    });
+    if (user && board) {
+        board.addUser(user);
+        res.send(true);
     } else {
-        response.send(false)
+        res.send(false)
     }
 })
 
-app.post('/api/board/:id/name', async (request, response) => { //Update Name of Board with that ID
-    if (request.params.id) {
-        let id = request.params.id;
-        let bName = request.body.bName;
-        let board = await Board.update({ title: bName }, {
-            where: {id: id}
-        })
-        response.send(true)
+app.post('/api/board/:id/removeuser/:userid', async (req, res) => { //Update Board -- Remove User
+    let board = await Board.findOne({
+        where: { id: req.params.id }
+    });
+    let user = await User.findOne({
+        where: { id: req.params.userid }
+    });
+    if (user && board) {
+        board.removeUser(user);
+        res.send(true);
     } else {
-        response.send(false)
+        res.send(false)
     }
 })
 
-app.post('/api/board/:id/delete', async (request, response) => { //Delete Board With that ID
-    if (request.params.id) {
-        let id = request.params.id;
-        await Board.destroy({
-            where: { id: id }
-        });
-        response.send(true);
-    } else {
-        response.send(false);
-    }
+app.post('/api/board/:id/title', async (req, res) => { //Update Title of Board with that ID
+    await Board.update({ title: req.body.title }, {
+        where: { id: req.params.id }
+    })
+    res.send(true)
+})
+
+app.post('/api/board/:id/delete', async (req, res) => { //Delete Board With that ID
+    await Board.destroy({
+        where: { id: req.params.id }
+    });
+    res.send(true)
 })
 
 //// Tasks
 
-app.get('/api/board/:id/tasks', async (request, response) => { // Get Tasks From the Board With that Board ID
-    if (request.params.id) {
-        let id = request.params.id;
-        let board = await Board.findOne({
-            where: { id: id }
-        });
-        let tasks = await board.getTasks();
-        response.send(tasks);
-    } else {
-        response.send(false);
-    }
+app.get('/api/board/:id/tasks', async (req, res) => { // Get Tasks From the Board With that Board ID
+    let board = await Board.findOne({
+        where: { id: req.params.id }
+    });
+    let tasks = await board.getTasks();
+    res.send(tasks);
 })
 
-app.post('/api/board/:id/tasks', async (request, response) => {// Create a New Task For the Board with that Board ID
-    const task = await new Task(request.body)
-    response.send(task)
+app.post('/api/board/:id/tasks', async (req, res) => {// Create a New Task For the Board with that Board ID
+    const task = await Task.create(req.body)
+    let id = req.params.id;
+    let board = await Board.findOne({
+        where: { id: id }
+    });
+    if (board) {
+        board.addTask(task)
+        if (req.body.userid) {
+            let user = await User.findOne({
+                where: { id: req.body.userid }
+            });
+            if (user) {
+                user.addTask(task)
+            }
+        }
+        res.send(true)
+    } else {
+        res.send(false)
+    }
 })
 
 //(Each task has a unique ID regardless of their board so we can just refrence it)
 
-app.get('/api/task/:taskid', async (request, response) => { // Get A Single Task 
-    if (request.params.taskid) {
-        let id = request.params.taskid;
-        let task = await Task.findOne({
-            where: { id: id }
-        });
-        response.send(task)
+app.get('/api/task/:taskid', async (req, res) => { // Get A Single Task 
+    let task = await Task.findOne({
+        where: { id: req.params.taskid }
+    });
+    res.send(task)
+})
+
+app.post('/api/task/:taskid', async (req, res) => {// Update a Specific Task with that Task ID
+    res.send(false)
+})
+
+app.post('/api/task/:taskid/name', async (req, res) => {// Update the Name of Specific Task with that Task ID
+    await Task.update({ name: req.body.name }, {
+        where: { id: req.params.taskid }
+    })
+    res.send(true)
+})
+
+app.post('/api/task/:taskid/assign/:userid', async (req, res) => {// Update the assigned User of Specific Task with that Task ID
+    const task = await Task.findByPk(req.params.taskid)
+    let user = await User.findOne({
+        where: { id: req.params.userid }
+    });
+    if (user && task) {
+        await task.setUser(user)
+        res.send(true)
     } else {
-        response.send(false)
+        res.send(false)
     }
 })
 
-app.post('/api/task/:taskid', async (request, response) => {// Update a Specific Task with that Task ID
-    const task = await Task.findByPk(request.params.taskid)
-    await task.update(request.body)
-    response.send(task)
-})
-
-app.post('/api/task/:taksid/description', (request, response) => {// Update the Description of Specific Task with that Task ID
-
-})
-
-app.post('/api/task/:taksid/assign', (request, response) => {// Update the assigned User of Specific Task with that Task ID
-
-})
-
-app.post('/api/task/:taksid/delete', (request, response) => { // Delete Task With that Task ID
-
+app.post('/api/task/:taskid/delete', async (req, res) => { // Delete Task With that Task ID
+    await Task.destroy({
+        where: { id: req.params.taskid }
+    });
+    res.send()
 })
 
 
