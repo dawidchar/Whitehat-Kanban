5ce5f27407e72026f5688c6f42a73e0a1cc35a80
dawidchar <dawidchar1@gmail.com>
2020-11-02 18:30:56 +0000

    Edit Task Works With Modal ✔

--- public/modalevents.js
+++ public/modalevents.js
@@ -2,8 +2,9 @@ $('#taskedit-modal').on('show.bs.modal', function (event) {
     var trigger = $(event.relatedTarget)
     var taskname = trigger.find('#taskname').text()
     var modal = $(this)
-    modal.find('#title').text('Edit' + taskname)
+    modal.find('#title').text('Edit ' + taskname)
     modal.find('#task-name').val(taskname)
+    modal.find('#task-id').val(trigger.parent().attr('id'))
 })
 
 $('#boardedit-modal').on('show.bs.modal', function (event) {
