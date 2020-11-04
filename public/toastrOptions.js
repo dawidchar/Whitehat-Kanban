d8d16601cfa36c5408c7f326501f07e19e6c8963
dawidchar <dawidchar1@gmail.com>
2020-11-04 14:39:47 +0000

    Toastr Options

---
+++ public/toastrOptions.js
@@ -0,0 +1,16 @@
+toastr.options = {
+    "closeButton": true,
+    "debug": false,
+    "newestOnTop": true,
+    "progressBar": false,
+    "positionClass": "toast-top-right",
+    "preventDuplicates": true,
+    "onclick": null,
+    "showDuration": "300",
+    "hideDuration": "1000",
+    "timeOut": "5000",
+    "extendedTimeOut": "1000",
+    "hideEasing": "swing",
+    "showMethod": "fadeIn",
+    "hideMethod": "fadeOut"
+  }
